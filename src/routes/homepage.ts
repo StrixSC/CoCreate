d8a0b1c1121ee1627cc5baa403813b8536d9f2ba
StrixSC <talon.strix@gmail.com>
2021-09-28 19:21:10 -0400

    testing deployment speed

--- src/routes/homepage.ts
+++ src/routes/homepage.ts
@@ -4,6 +4,6 @@ import { StatusCodes } from 'http-status-codes';
 
 export const WelcomeMessage = (req: Request, res: Response) => {
     res.status(StatusCodes.OK).send({
-        'message': 'Test Message!'
+        'message': 'New Message Message! 2'
     })
 }
