f522653afb952d40c2d5fa062c29370abf27bedb
StrixSC <talon.strix@gmail.com>
2021-10-07 15:50:26 -0400

    Fixed docker compose file to work on ports 3000, 3001, 3002 & added db init script to postgres container

---
+++ sql/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS Users (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL
+);
