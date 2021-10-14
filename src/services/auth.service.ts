2f28d5cafdf53f07d09030a4787b25c3900a0462
StrixSC <talon.strix@gmail.com>
2021-10-14 03:11:12 -0400

    Changed token format on login and registration and updated swagger.json

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -7,47 +7,47 @@ import { Prisma } from '.prisma/client';
 import { ISignedJWTResponse } from '../models/ISignedJWTPayload.model';
 
 const authErrorRouters: { [key: string]: HttpError } = {
-    'P2001': new create.Unauthorized("Unauthorized"),
-    'P2002': new create.Conflict("Email already in use")
+  'P2001': new create.Unauthorized('Unauthorized'),
+  'P2002': new create.Conflict('Email already in use')
 };
 
 export const login = async (email: string, password: string): Promise<ISignedJWTResponse> => {
-    const user = await db.user.findUnique({
-        where: { email }
-    });
+  const user = await db.user.findUnique({
+    where: { email }
+  });
 
-    if (!user)
-        throw new create.Unauthorized("User was not found");
+  if (!user)
+    throw new create.Unauthorized('User was not found');
 
-    const checkPassword = compareSync(password, user.password);
-    if (!checkPassword) throw new create.Unauthorized("Invalid email address or password.");
+  const checkPassword = compareSync(password, user.password);
+  if (!checkPassword) throw new create.Unauthorized('Invalid email address or password.');
 
-    return signToken(user);
-}
+  return signToken(user);
+};
 
 export const register = async (email: string, password: string): Promise<ISignedJWTResponse> => {
 
-    if (!validateEmail(email) || !validatePassword(password))
-        throw new create.BadRequest("Invalid format of email or password");
+  if (!validateEmail(email) || !validatePassword(password))
+    throw new create.BadRequest('Invalid format of email or password');
 
-    const lowercaseEmail = (email as string).toLowerCase();
-    const hashedPassword = hashSync(password, 10);
+  const lowercaseEmail = (email as string).toLowerCase();
+  const hashedPassword = hashSync(password, 10);
 
-    try {
-        const user = await db.user.create({
-            data: {
-                email: lowercaseEmail,
-                password: hashedPassword
-            }
-        });
+  try {
+    const user = await db.user.create({
+      data: {
+        email: lowercaseEmail,
+        password: hashedPassword
+      }
+    });
 
-        if (!user) throw new create.InternalServerError("Internal Server Error");
+    if (!user) throw new create.InternalServerError('Internal Server Error');
 
-        return signToken(user);
-    } catch (e: any) {
-        if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            throw authErrorRouters[e.code] || new create.InternalServerError("Internal Server Error");
-        }
-        throw e;
+    return signToken(user);
+  } catch (e: any) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      throw authErrorRouters[e.code] || new create.InternalServerError('Internal Server Error');
     }
-}
+    throw e;
+  }
+};
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,27 +1,27 @@
-import { JwtPayload, sign } from "jsonwebtoken";
-import create from "http-errors";
-import { User } from ".prisma/client";
-import { ISignedJWTResponse } from "../models/ISignedJWTPayload.model";
+import { JwtPayload, sign } from 'jsonwebtoken';
+import create from 'http-errors';
+import { User } from '.prisma/client';
+import { ISignedJWTResponse } from '../models/ISignedJWTPayload.model';
 
 const secret = process.env.ACCESS_TOKEN_SECRET;
 
 export const signToken = (user: User): ISignedJWTResponse => {
-    try {
+  try {
 
-        const payload: JwtPayload = {
-            sub: user.user_id,
-            iat: Date.now()
-        }
-        const expiresIn = process.env.TOKEN_EXPIRY_TIME || "1d";
-        const token = sign(payload, secret || 'secret', { expiresIn: expiresIn });
+    const payload: JwtPayload = {
+      sub: user.user_id,
+      iat: Date.now()
+    };
+    const expiresIn = process.env.TOKEN_EXPIRY_TIME || '1d';
+    const token = sign(payload, secret || 'secret', { expiresIn: expiresIn });
 
-        return {
-            token: "Bearer " + token,
-            expires: expiresIn
-        }
+    return {
+      token: 'Bearer ' + token,
+      expires: expiresIn
+    };
 
-    } catch (e: any) {
-        console.error(e);
-        throw new create.InternalServerError("Unexpected error");
-    }
-}
+  } catch (e: any) {
+    console.error(e);
+    throw new create.InternalServerError('Unexpected error');
+  }
+};
