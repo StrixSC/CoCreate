6c2ff157f71a9bd08748becbba8f3a45f016ea12
StrixSC <talon.strix@gmail.com>
2021-10-25 17:02:48 -0400

    Test server

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,91 +1,97 @@
 #!/usr/bin/env node
-import 'reflect-metadata';
-import Debug from 'debug';
-import http from 'http';
-import { app, expressSession } from '../app';
-import { Server, Socket } from 'socket.io';
-import corsOptions from '../cors';
-import { checkAuthenticated } from './../middlewares/auth.middleware';
+import "reflect-metadata";
+import Debug from "debug";
+import http from "http";
+import { app, expressSession } from "../app";
+import { Server, Socket } from "socket.io";
+import corsOptions from "../cors";
+import { checkAuthenticated } from "./../middlewares/auth.middleware";
 
 // Events
-import chatHandler from '../events/chat.events';
-import passport from 'passport';
-import chalk from 'chalk';
+import chatHandler from "../events/chat.events";
+import drawingHandler from "../events/drawing.events";
+import passport from "passport";
+import chalk from "chalk";
 
 export const normalizePort = (val: string) => {
-    const port = parseInt(val, 10);
-    if (Number.isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
+  const port = parseInt(val, 10);
+  if (Number.isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
 };
 
-const debug = Debug('Colorimage');
-const port = normalizePort(process.env.PORT || '3000');
+const debug = Debug("Colorimage");
+const port = normalizePort(process.env.PORT || "3000");
 const server = http.createServer(app);
 
 const onError = (error: any) => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(`${bind} requires elevated privileges`);
-            process.exit(1);
-        case 'EADDRINUSE':
-            console.error(`${bind} is already in use`);
-            process.exit(1);
-        default:
-            throw error;
-    }
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+    default:
+      throw error;
+  }
 };
 
 const onListening = () => {
-    const addr = server.address();
-    const bind =
-        typeof addr === 'string'
-            ? `pipe ${addr}`
-            : `port ${addr ? addr.port : 'ERR: Address is null'}`;
+  const addr = server.address();
+  const bind =
+    typeof addr === "string"
+      ? `pipe ${addr}`
+      : `port ${addr ? addr.port : "ERR: Address is null"}`;
 
-    debug(`Listening on ${bind}`);
+  debug(`Listening on ${bind}`);
 };
 
 const io = new Server(server, {
-    cors: corsOptions
+  cors: corsOptions,
 });
 
 const onConnection = (socket: Socket) => {
-    chatHandler(io, socket);
-    socket.on('connect_error', (err: any) => {
-        console.log(`connect_error due to ${err.message}`);
-    });
+  chatHandler(io, socket);
+  drawingHandler(io, socket);
+  socket.on("connect_error", (err: any) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
 };
 
 const wrap = (middleware: any) => (socket: Socket, next: any) =>
-    middleware(socket.request, {}, next);
+  middleware(socket.request, {}, next);
 
-io.use(wrap(expressSession));
-io.use(wrap(passport.initialize()));
-io.use(wrap(passport.session()));
-io.use(wrap(checkAuthenticated));
+// io.use(wrap(expressSession));
+// io.use(wrap(passport.initialize()));
+// io.use(wrap(passport.session()));
+// io.use(wrap(checkAuthenticated));
 
-io.on('connection', onConnection);
-io.of('/').adapter.on('create-room', (room) => {
-    console.log(chalk.greenBright(`[SOCKET]::EventTriggered:: Room ${room} was created.`));
+io.on("connection", onConnection);
+io.of("/").adapter.on("create-room", (room) => {
+  console.log(
+    chalk.greenBright(`[SOCKET]::EventTriggered:: Room ${room} was created.`)
+  );
 });
 
-io.of('/').adapter.on('join-room', (room, id) => {
-    console.log(
-        chalk.greenBright(`[SOCKET]::EventTriggered:: Room ${room} was joined by socket ${id}.`)
-    );
+io.of("/").adapter.on("join-room", (room, id) => {
+  console.log(
+    chalk.greenBright(
+      `[SOCKET]::EventTriggered:: Room ${room} was joined by socket ${id}.`
+    )
+  );
 });
 
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
 export { io };
--- src/events/chat.events.ts
+++ src/events/chat.events.ts
@@ -1,114 +1,128 @@
-import { getSinglePublicProfileById } from './../services/users.service';
-import { Prisma } from '.prisma/client';
-import create from 'http-errors';
-import { ISendMessagePayload } from '../models/ISendMessagePayload.model';
-import { Server, Socket } from 'socket.io';
-import { IReceiveMessagePayload } from '../models/IReceiveMessagePayload.model';
+import { getSinglePublicProfileById } from "./../services/users.service";
+import { Prisma } from ".prisma/client";
+import create from "http-errors";
+import { ISendMessagePayload } from "../models/ISendMessagePayload.model";
+import { Server, Socket } from "socket.io";
+import { IReceiveMessagePayload } from "../models/IReceiveMessagePayload.model";
 import {
-    addUserMessage,
-    joinChannel as addUserToChannel,
-    leaveChannel as removeUserFromChannel
-} from './../services/channels.service';
-import moment from 'moment';
-import { IChannelIOPayload } from '../models/IJoinChannelPayload.model';
-import { dbErrorRouters } from '../utils/auth';
-import chalk from 'chalk';
+  addUserMessage,
+  joinChannel as addUserToChannel,
+  leaveChannel as removeUserFromChannel,
+} from "./../services/channels.service";
+import moment from "moment";
+import { IChannelIOPayload } from "../models/IJoinChannelPayload.model";
+import { dbErrorRouters } from "../utils/auth";
+import chalk from "chalk";
 
 export = (io: Server, socket: Socket) => {
-    console.log(chalk.greenBright('[INFO]::Socket Event Triggered::'));
-    const userId = (socket as any).request.session.passport.user;
-    const joinChannel = async (joinChannelPayload: IChannelIOPayload) => {
-        try {
-            const member = await addUserToChannel({ ...joinChannelPayload, userId: userId });
-            if (!member) {
-                throw new create.InternalServerError('Could not add user to channel.');
-            }
+  // console.log(chalk.greenBright('[INFO]::Socket Event Triggered::'));
+  // const userId = (socket as any).request.session.passport.user;
+  const userId = "test";
+  const joinChannel = async (joinChannelPayload: IChannelIOPayload) => {
+    try {
+      const member = await addUserToChannel({
+        ...joinChannelPayload,
+        userId: userId,
+      });
+      if (!member) {
+        throw new create.InternalServerError("Could not add user to channel.");
+      }
 
-            socket.join(joinChannelPayload.channelId);
-            io.to(joinChannelPayload.channelId).emit('user-connection', {
-                username: 'Système',
-                message_data: `${member.username} s'est connecté! 😄`,
-                timestamp: moment().format('HH:mm:ss'),
-                avatar_url: '',
-                message_id: 'SystemMessage',
-                sender_profile_id: 'SYSTEM'
-            } as IReceiveMessagePayload);
-        } catch (e: any) {
-            return handleError(e);
-        }
-    };
+      socket.join(joinChannelPayload.channelId);
+      io.to(joinChannelPayload.channelId).emit("user-connection", {
+        username: "Système",
+        message_data: `${member.username} s'est connecté! 😄`,
+        timestamp: moment().format("HH:mm:ss"),
+        avatar_url: "",
+        message_id: "SystemMessage",
+        sender_profile_id: "SYSTEM",
+      } as IReceiveMessagePayload);
+    } catch (e: any) {
+      return handleError(e);
+    }
+  };
 
-    const sendMessage = async (messagePayload: ISendMessagePayload) => {
-        try {
-            const message = await addUserMessage({ ...messagePayload, user_id: userId });
-            if (!message) throw new create.InternalServerError('Could not find the message');
+  const sendMessage = async (messagePayload: ISendMessagePayload) => {
+    try {
+      const message = await addUserMessage({
+        ...messagePayload,
+        user_id: userId,
+      });
+      if (!message)
+        throw new create.InternalServerError("Could not find the message");
 
-            io.to(messagePayload.channel_id).emit('receive-message', message);
-        } catch (e: any) {
-            return handleError(e);
-        }
-    };
+      io.to(messagePayload.channel_id).emit("receive-message", message);
+    } catch (e: any) {
+      return handleError(e);
+    }
+  };
 
-    const leaveChannel = async (payload: IChannelIOPayload) => {
-        try {
-            const member = await removeUserFromChannel({
-                ...payload,
-                userId: userId
-            });
+  const leaveChannel = async (payload: IChannelIOPayload) => {
+    try {
+      const member = await removeUserFromChannel({
+        ...payload,
+        userId: userId,
+      });
 
-            if (!member)
-                throw new create.InternalServerError('Could not remove user from channel.');
+      if (!member)
+        throw new create.InternalServerError(
+          "Could not remove user from channel."
+        );
 
-            io.to(payload.channelId).emit('user-disconnect', {
-                username: 'Système',
-                message: `${member.username} s'est déconnecté... 😭`,
-                timestamp: moment().format('HH:mm:ss'),
-                avatar_url: '',
-                message_id: 'SystemMessage',
-                sender_profile_id: 'SYSTEM'
-            });
-        } catch (e: any) {
-            return handleError(e);
-        }
-    };
+      io.to(payload.channelId).emit("user-disconnect", {
+        username: "Système",
+        message: `${member.username} s'est déconnecté... 😭`,
+        timestamp: moment().format("HH:mm:ss"),
+        avatar_url: "",
+        message_id: "SystemMessage",
+        sender_profile_id: "SYSTEM",
+      });
+    } catch (e: any) {
+      return handleError(e);
+    }
+  };
 
-    const userDisconnect = async (reason: string) => {
-        try {
-            const member = await getSinglePublicProfileById(userId);
-            if (!member) {
-                throw new create.InternalServerError('Could not retrieve member.');
-            }
+  const userDisconnect = async (reason: string) => {
+    try {
+      const member = await getSinglePublicProfileById(userId);
+      if (!member) {
+        throw new create.InternalServerError("Could not retrieve member.");
+      }
 
-            socket.rooms.forEach((room) => {
-                socket.to(room).emit('user-disconnect', {
-                    username: 'Système',
-                    message: `${member.username} s'est déconnecté... 😭`,
-                    timestamp: moment().format('HH:mm:ss'),
-                    avatar_url: '',
-                    message_id: 'SystemMessage',
-                    sender_profile_id: 'SYSTEM'
-                });
-            });
+      socket.rooms.forEach((room) => {
+        socket.to(room).emit("user-disconnect", {
+          username: "Système",
+          message: `${member.username} s'est déconnecté... 😭`,
+          timestamp: moment().format("HH:mm:ss"),
+          avatar_url: "",
+          message_id: "SystemMessage",
+          sender_profile_id: "SYSTEM",
+        });
+      });
 
-            socket.emit('self-disconnect', {
-                message: reason
-            });
-        } catch (e: any) {
-            return handleError(e);
-        }
-    };
+      socket.emit("self-disconnect", {
+        message: reason,
+      });
+    } catch (e: any) {
+      return handleError(e);
+    }
+  };
 
-    const handleError = (e: any): boolean => {
-        if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const error = dbErrorRouters[e.code];
-            if (error) return socket.emit('exception', create(error.statusCode, error.message));
-            else return socket.emit('exception', create(create.InternalServerError));
-        }
-        return socket.emit('exception', create(e.status, e.message));
-    };
+  const handleError = (e: any): boolean => {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      const error = dbErrorRouters[e.code];
+      if (error)
+        return socket.emit(
+          "exception",
+          create(error.statusCode, error.message)
+        );
+      else return socket.emit("exception", create(create.InternalServerError));
+    }
+    return socket.emit("exception", create(e.status, e.message));
+  };
 
-    socket.on('send-message', sendMessage);
-    socket.on('join-channel', joinChannel);
-    socket.on('leave-channel', leaveChannel);
-    socket.on('disconnect', userDisconnect);
+  socket.on("send-message", sendMessage);
+  socket.on("join-channel", joinChannel);
+  socket.on("leave-channel", leaveChannel);
+  socket.on("disconnect", userDisconnect);
 };
