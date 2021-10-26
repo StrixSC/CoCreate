8e64e23b8bc4dee5ac79760db4de3fe70fa6f13a
paama <ama.maribel@outlook.fr>
2021-10-26 09:22:04 -0400

    Redirecting to drawing workspace

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
-import { ChatGuard } from './guards/chat.guard';
-import { ChatComponent } from './components/chat/chat.component';
+//import { ChatGuard } from './guards/chat.guard';
+///import { ChatComponent } from './components/chat/chat.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent } from './components/login-page/login-page.component';
+//import { LoginPageComponent } from './components/login-page/login-page.component';
 import { DrawingPageComponent } from './components/drawing-page/drawing-page.component';
-import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
-import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
-import { WorkspaceComponent } from './components/workspace/workspace.component';
+//import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+//import { SignUpPageComponent } from './components/sign-up-page/sign-up-page.component';
+//import { WorkspaceComponent } from './components/workspace/workspace.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "login", pathMatch: "full" },
-  { path: "login", component: LoginPageComponent },
-  { path: "drawing", component: DrawingPageComponent, canActivate: [ChatGuard] },
-  { path: "chat", component: ChatComponent},
+  { path: "", redirectTo: "drawing", pathMatch: "full" },
+  /*{ path: "login", component: LoginPageComponent },*/
+  { path: "drawing", component: DrawingPageComponent},
+  /*{ path: "chat", component: ChatComponent},
   { path: 'home', component: WelcomePageComponent },
   { path: 'signup', component: SignUpPageComponent },
-  { path: 'workspace', component: WorkspaceComponent}
+  { path: 'workspace', component: WorkspaceComponent}*/
 
   /*{ path: "**", redirectTo: "chat" }*/
 ];
