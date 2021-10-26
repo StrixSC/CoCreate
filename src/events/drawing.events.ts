3550477e11dab8a972c7cb54fdfd702949bb498e
StrixSC <talon.strix@gmail.com>
2021-10-26 01:45:45 -0400

    Added testing socket server on port 6000 for drawing events without authentication required

--- src/events/drawing.events.ts
+++ src/events/drawing.events.ts
@@ -1,16 +1,15 @@
 import chalk from 'chalk';
+import { debug } from 'console';
 import { Server, Socket } from 'socket.io';
 
 export = (io: Server, socket: Socket) => {
-    console.log(chalk.greenBright('[INFO]::Socket Event Triggered::'));
-    //const userId = (socket as any).request.session.passport.user;
-
-    const sendCoords = (coords: { x: number; y: number; state: string }) => {
-        console.log(`${socket.id} moved to ${coords.x}, ${coords.y}`);
+    const sendCoords = (data: { x: number; y: number; state: string; actionId: string }) => {
+        debug(chalk.blueBright(`[SOCKET_TESTING]::${socket.id} moved to ${data.x}, ${data.y}`));
         io.emit('freedraw:receive', {
-            x: coords.x,
-            y: coords.y,
-            state: coords.state // down, move, up
+            x: data.x,
+            y: data.y,
+            state: data.state, // down, move, up
+            actionId: data.actionId
         });
     };
 
