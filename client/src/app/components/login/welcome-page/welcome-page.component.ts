1fd7682f479d0e017f5f7ab9ef71d4c923f60ff0
Ragib Ahashan <ragibahashan@gmail.com>
2021-10-26 17:41:22 -0400

    Sign in works

--- client/src/app/components/login/welcome-page/welcome-page.component.ts
+++ client/src/app/components/login/welcome-page/welcome-page.component.ts
@@ -1,38 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { UserService } from 'src/app/services/user.service';
-import { User } from '../../../../../../common/communication/user.model';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { UserService } from "src/app/services/user.service";
+import { User } from "../../../../../../common/communication/user.model";
+import axios from "axios";
 
 @Component({
-  selector: 'app-welcome-page',
-  templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.scss']
+  selector: "app-welcome-page",
+  templateUrl: "./welcome-page.component.html",
+  styleUrls: ["./welcome-page.component.scss"],
 })
 export class WelcomePageComponent implements OnInit {
-  
-  user=new User();
-  constructor(public userService:UserService) { }
+  user = new User();
 
-  ngOnInit() {
+  constructor(public userService: UserService) {}
+
+  ngOnInit() {}
+
+  mySubmit(f: NgForm) {
+    this.signIn();
   }
-  resetForm(form?:NgForm) {
-    if(form != null)
-    form.reset();
-    this.user = {
-      username: '',
-      email:'',
-      firstName:'',
-      lastName:'',
-      password:''
 
+  async signIn() {
+    console.log(this.user);
+    const URL = "http://localhost:3000/auth/login";
+    const PAYLOAD = {
+      email: this.user.email,
+      password: this.user.password,
+    };
+    const headers = {
+      "Content-Type": "application/json",
+    };
+    try {
+      const res = await axios.post(URL, PAYLOAD, { headers: headers });
+      console.log(res);
+    } catch (error) {
+      console.error(error);
     }
   }
-  mySubmit(f:NgForm){
-    console.log("submitted")
-    console.log(JSON.stringify(this.user))
-    this.userService.loginUser(JSON.stringify(this.user)).subscribe((t) => {
-      console.log(t)
-    });
-  } 
-
 }
