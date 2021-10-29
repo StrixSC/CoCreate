94fac2cd8764ad7e18f359ef4bd426fcb0a407c9
Ragib Ahashan <ragibahashan@gmail.com>
2021-10-29 17:17:06 -0400

    bug fix

--- client/src/app/services/chat/socket.service.ts
+++ client/src/app/services/chat/socket.service.ts
@@ -16,9 +16,10 @@ export class SocketService {
   }
 
   setupSocketConnection(ip?: string): void {
-    this.socket = io(environment.WS_URL || "http://localhost:3000", {
+    this.socket = io(environment.WS_URL || "http://localhost:5000", {
       autoConnect: false,
     }) as Socket;
+    console.log("this.socket");
   }
 
   connect(): void {
