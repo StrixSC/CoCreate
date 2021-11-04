9b3f54dee874bf587ba8a4e49fc59b627efd17dc
StrixSC <talon.strix@gmail.com>
2021-11-04 15:03:49 -0400

    Added procfile and better remote debugging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,6 +3,18 @@ import chalk from 'chalk';
 type DebugType = 'ERROR' | 'INFO' | 'WARNING' | 'SUCCESS' | 'CRITICAL' | 'DEBUG';
 const debug = Debug('Colorimage');
 
+// Create a logger instance with Winston.
+import winston from 'winston';
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: './logs/combined.log' })
+    ]
+});
+
 const colors: { [key: string]: (message?: any, ...optionalParams: any[]) => any } = {
     ERROR: chalk.bold.redBright,
     INFO: chalk.blueBright,
@@ -14,4 +26,5 @@ const colors: { [key: string]: (message?: any, ...optionalParams: any[]) => any 
 
 export default function log(type: DebugType, ...messages: any[]) {
     debug(colors[type?.toString() || 'INFO'](`[${type || 'INFO'}]`, ...messages));
+    logger.info(`[${type || 'INFO'}]`, ...messages);
 }
