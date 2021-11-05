914c265c18b60f4de17b9418e0a69f79f726a8d4
StrixSC <talon.strix@gmail.com>
2021-11-05 17:29:45 -0400

    Removed access control allow origin flag wildcard

--- src/app.ts
+++ src/app.ts
@@ -71,7 +71,7 @@ app.use(corsSetup);
 
 app.options('*', corsSetup);
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
     res.header(
