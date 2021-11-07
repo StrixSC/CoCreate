604a647d2225321234d8063f61a205bddfd1c2e0
StrixSC <talon.strix@gmail.com>
2021-11-07 03:29:14 -0500

    Modified seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,60 +15,6 @@ async function main() {
         }
     });
 
-    const systemUser = await prisma.user.upsert({
-        where: {
-            user_id: 'ADMIN'
-        },
-        update: {},
-        create: {
-            user_id: 'ADMIN',
-            email: 'admin',
-            password: '$2b$10$96mpoqKrG5rRdLKm16p9U.g9TQZNXjuLOXqRIZ4eVNwLC/tk.ajCe', // admin
-            profile: {
-                create: {
-                    username: 'admin',
-                    avatar_url: ''
-                }
-            },
-            account: {
-                create: {
-                    first_name: 'Admin',
-                    last_name: 'Admin'
-                }
-            },
-            channels: {
-                create: [ { channel_id: 'PUBLIC', type: MemberType.Owner } ]
-            }
-        }
-    });
-
-    const demoUser = await prisma.user.upsert({
-        where: {
-            user_id: 'DEMO'
-        },
-        update: {},
-        create: {
-            user_id: 'DEMO',
-            email: 'demo',
-            password: '$2b$10$BCGXlbIMN7mg0jXZ4lFpV.JQ5AqNSROSjqvhDN.ZQVf0P.ku20Lem', // demo
-            profile: {
-                create: {
-                    username: 'demo',
-                    avatar_url: ''
-                }
-            },
-            account: {
-                create: {
-                    first_name: 'demo',
-                    last_name: 'demo'
-                }
-            },
-            channels: {
-                create: [ { channel_id: 'PUBLIC', type: MemberType.Regular } ]
-            }
-        }
-    });
-
     const demoCollaboration = await prisma.collaboration.upsert({
         where: {
             collaboration_id: 'DEMO_COLLABORATION'
@@ -78,7 +24,7 @@ async function main() {
             collaboration_id: 'DEMO_COLLABORATION',
             drawing_id: 'DEMO_DRAWING',
             collaboration_members: {
-                create: [ { user_id: 'DEMO', type: 'Owner' } ]
+                create: [{ user_id: 'DEMO', type: 'Owner' }]
             },
             drawing: {
                 create: {
