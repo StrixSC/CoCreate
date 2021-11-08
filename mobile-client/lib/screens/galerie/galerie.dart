b3239d27dea0e20593ae10f285891d9b52338639
Pritam Patel <pritam.patel@polymtl.ca>
2021-11-08 17:18:59 -0500

    Added search and create option for galerie with UI improvements

--- mobile-client/lib/screens/galerie/galerie.dart
+++ mobile-client/lib/screens/galerie/galerie.dart
@@ -1,7 +1,11 @@
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Galerie extends StatefulWidget {
+const _fontSize = 20.0;
+const padding = 30.0;
 
+class Galerie extends StatefulWidget {
   @override
   GalerieState createState() => GalerieState();
 }
@@ -12,6 +16,11 @@ class GalerieState extends State<Galerie> {
   int pageCount = 1;
   List<_Photo> _dessins = [];
   late ScrollController _scrollController;
+  static final GlobalKey<FormFieldState<String>> _searchFormKey =
+      GlobalKey<FormFieldState<String>>();
+  final FocusNode _focusNode = FocusNode();
+
+  TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
@@ -34,33 +43,106 @@ class GalerieState extends State<Galerie> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text("Galerie de dessins"),
-      ),
-      body: GridView.count(
-        controller: _scrollController,
-        scrollDirection: Axis.vertical,
-        restorationId: 'grid_offset',
-        crossAxisCount: 2,
-        mainAxisSpacing: 8,
-        crossAxisSpacing: 8,
-        padding: const EdgeInsets.all(8),
-        childAspectRatio: 1,
-        physics: const AlwaysScrollableScrollPhysics(),
-        children: dataList.map((value) {
-          return _GridDemoPhotoItem(
-            photo: value,
-          );
+        appBar: AppBar(
+            automaticallyImplyLeading: false,
+            title: const Text("Galerie de dessins"),
+            actions: <Widget>[
+              IconButton(
+                  icon: const Icon(CupertinoIcons.plus,
+                      color: Colors.white, size: 34),
+                  onPressed: () {createDessinDialog();
+                  })
+            ]),
+        body: Container(
+          color: Colors.white,
+          child: Column(children: <Widget>[
+            const SizedBox(height: 24.0),
+            Container(
+                width: 500.0,
+                child: TextField(
+                  key: _searchFormKey,
+                  style: const TextStyle(fontSize: 25),
+                  controller: searchController,
+                  focusNode: _focusNode,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    errorStyle: const TextStyle(fontSize: 26),
+                    hintText: "Filtrer les dessins selon un attribut",
+                    hintStyle: const TextStyle(
+                      fontSize: 26,
+                    ),
+                    contentPadding: const EdgeInsets.all(15),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(0)),
+                    prefixIcon: const Icon(Icons.search),
+                  ),
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  autofocus: false,
+                )),
+            const SizedBox(height: 24.0),
+            Expanded(
+                child: GridView.count(
+              controller: _scrollController,
+              childAspectRatio: 3 / 2,
+              scrollDirection: Axis.vertical,
+              restorationId: 'grid_offset',
+              crossAxisCount: 2,
+              mainAxisSpacing: 18,
+              crossAxisSpacing: 18,
+              padding: const EdgeInsets.all(8),
+              physics: const AlwaysScrollableScrollPhysics(),
+              children: dataList.map((value) {
+                return _GridDemoPhotoItem(
+                  photo: value,
+                );
+              }).toList(),
+            ))
+          ]),
+        ));
+  }
 
-        }).toList(),
-      ),
-    );
+  createDessinDialog() async {
+    showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text(
+              'Créer un dessin'),
+          content: TextFormField(
+            style: const TextStyle(fontSize: _fontSize),
+            maxLines: 1,
+            autofocus: false,
+            decoration: InputDecoration(
+              errorStyle: const TextStyle(fontSize: _fontSize),
+              hintText: "Courriel",
+              hintStyle: const TextStyle(
+                fontSize: _fontSize,
+              ),
+              contentPadding: const EdgeInsets.all(15.0),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12.0)),
+            ),
+            autovalidate: true,
+            // onFieldSubmitted: (value) {
+            //   if (_formKey.currentState!.validate()) {
+            //     _onSubmitTap(context, userController.text);
+            //   }
+            // },
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, 'Créer');
+              },
+              child: const Text('Créer'),
+            ),
+          ],
+        ));
   }
 
   _scrollListener() {
     if (_scrollController.offset >=
-        _scrollController.position.maxScrollExtent &&
+            _scrollController.position.maxScrollExtent &&
         !_scrollController.position.outOfRange) {
       setState(() {
         print("comes to bottom $isLoading");
@@ -129,29 +211,33 @@ class _GridDemoPhotoItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final Widget image = Material(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
-      clipBehavior: Clip.antiAlias,
-      child: Image.asset(
-        photo.assetName,
-        fit: BoxFit.cover,
-      ),
-    );
-
-        return GridTile(
-          footer: Material(
-            color: Colors.transparent,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(4)),
-            ),
-            clipBehavior: Clip.antiAlias,
-            child: GridTileBar(
-              backgroundColor: Colors.black45,
-              title: _GridTitleText(photo.title),
-              subtitle: _GridTitleText(photo.subtitle),
-            ),
+    final Widget image = Container(
+        decoration: BoxDecoration(
+            border:
+                Border.all(width: 2.5, color: Colors.grey.withOpacity(0.2))),
+        child: Material(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(3)),
+          clipBehavior: Clip.antiAlias,
+          child: Image.asset(
+            photo.assetName,
+            fit: BoxFit.cover,
           ),
-          child: image,
-        );
-    }
+        ));
+
+    return GridTile(
+      footer: Material(
+        color: Colors.transparent,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(3)),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: GridTileBar(
+          backgroundColor: Colors.black45,
+          title: _GridTitleText(photo.title),
+          subtitle: _GridTitleText(photo.subtitle),
+        ),
+      ),
+      child: image,
+    );
   }
+}
