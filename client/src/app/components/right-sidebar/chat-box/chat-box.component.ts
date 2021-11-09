cacf8c3bd2b70fd2de06b506b06c8b016100d5e2
RagibAhashan <ahashanragib@gmail.com>
2021-11-09 16:43:44 -0500

    message bubbles are here now

--- client/src/app/components/right-sidebar/chat-box/chat-box.component.ts
+++ client/src/app/components/right-sidebar/chat-box/chat-box.component.ts
@@ -1,15 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ThemePalette } from "@angular/material/core";
+
+export interface Messages {
+  text: string;
+  color: ThemePalette;
+  isMe: boolean;
+  style: {
+    height: string;
+    width: string;
+    margin?: string;
+    right?: string;
+  };
+}
 
 @Component({
-  selector: 'app-chat-box',
-  templateUrl: './chat-box.component.html',
-  styleUrls: ['./chat-box.component.scss']
+  selector: "app-chat-box",
+  templateUrl: "./chat-box.component.html",
+  styleUrls: ["./chat-box.component.scss"],
 })
 export class ChatBoxComponent implements OnInit {
+  socketConversation: Messages[] = [
+    {
+      text: "In some scenarios, developers might prefer that behavior over the default and would like to have the same for",
+      color: "accent",
+      isMe: false,
+      style: {
+        height: "50px",
+        width: "300px",
+        margin: "0 5 5 0",
+      },
+    },
 
-  constructor() { }
+    {
+      text: "In some scenarios",
+      color: "warn",
+      isMe: true,
+      style: {
+        height: "50px",
+        width: "100px",
+        right: "1",
+      },
+    },
+  ];
 
-  ngOnInit() {
+  chatBoxCSS: any = {};
+
+  isChatOpen = true;
+
+  constructor() {
+    this.openChatBox();
   }
 
+  sendMessage() {
+    this.socketConversation.push({
+      text: "In some scenarios",
+      color: "warn",
+      isMe: true,
+      style: {
+        height: "50px",
+        width: "100px",
+        right: "1",
+      },
+    });
+  }
+
+  ngOnInit() {}
+
+  openChatBox() {
+    this.chatBoxCSS = {
+      width: "400px",
+      height: "450px",
+      "background-color": "rgb(247, 247, 247)",
+      position: "fixed",
+      bottom: "0",
+      right: "300px",
+      border: "2px solid black",
+      "border-top-left-radius": "5%",
+      "border-top-right-radius": "5%",
+      "border-bottom-left-radius": "5%",
+      overflow: "hidden",
+    };
+    this.isChatOpen = true;
+  }
+
+  minimiseChatBox() {
+    this.chatBoxCSS = {
+      width: "350px",
+      height: "40px",
+      "background-color": "rgb(247, 247, 247)",
+      position: "fixed",
+      bottom: "0",
+      right: "300px",
+      border: "2px solid black",
+    };
+    this.isChatOpen = false;
+  }
 }
