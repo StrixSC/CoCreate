df5222a3fdc11cf4c1ce8c9358b1a746103a799d
paama <ama.maribel@outlook.fr>
2021-11-15 15:36:36 -0500

    Add option to form

--- client/src/app/components/new-drawing/new-drawing-form/new-drawing-form.component.ts
+++ client/src/app/components/new-drawing/new-drawing-form/new-drawing-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { VisibilityType } from 'src/app/model/drawing-visibility.model';
 
 const X_SIZE: { width: number, height: number, x: number, y: number } = { width: 12, height: 12, x: 1, y: 11 };
 const MIN_DIMENSION = 1;
@@ -15,5 +16,15 @@ export class NewDrawingFormComponent {
   /// Cree un nouveau form pour la grosseur
   @Input()
   sizeForm: FormGroup;
+
+
+    
+    public visibilityTypes: String[];
+    public teamNames: String[];
+    public constructor(){
+      
+      this.visibilityTypes = [VisibilityType.public, VisibilityType.protected, VisibilityType.private]
+      this.teamNames = ['Moi-même', 'Équipe de collaboration']
+    }
 
 }
