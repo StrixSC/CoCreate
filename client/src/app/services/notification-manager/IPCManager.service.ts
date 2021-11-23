fc74899e032270a1536fbb19e96f277a8571061f
RagibAhashan <ahashanragib@gmail.com>
2021-11-22 21:48:19 -0500

    removing comments

--- client/src/app/services/notification-manager/IPCManager.service.ts
+++ client/src/app/services/notification-manager/IPCManager.service.ts
@@ -1,29 +1,5 @@
 import { Injectable } from "@angular/core";
-import { contextBridge, IpcRenderer, ipcRenderer } from "electron";
-
-// export type Api = {
-//   send: (channel: string, ...args: any[]) => void;
-//   invoke: (channel: string, ...args: any[]) => Promise<any>;
-//   on: (channel: string, listener: any) => void;
-//   once: (channel: string, listener: any) => void;
-//   removeAllListeners: (channel: string) => void;
-// };
-
-// declare global {
-//   interface Window {
-//     ElectronBridge: Api;
-//   }
-// }
-
-// contextBridge.exposeInMainWorld("ElectronBridge", {
-//   send: (channel: string, ...args: any[]) => ipcRenderer.send(channel, ...args),
-//   invoke: (channel: string, ...args: any[]) =>
-//     ipcRenderer.invoke(channel, ...args),
-//   on: (channel: string, listener: any) => ipcRenderer.on(channel, listener),
-//   once: (channel: string, listener: any) => ipcRenderer.once(channel, listener),
-//   removeAllListeners: (channel: string) =>
-//     ipcRenderer.removeAllListeners(channel),
-// });
+import { IpcRenderer } from "electron";
 
 declare var window: Window;
 declare global {
